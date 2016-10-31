29ef361440649a529ed18f36776b269d70bad907
Lakshmi Bomma <lbomma@riis.com>
2016-10-31 16:35:26 -0400

    class creation, constructor creation, static properties,static method, instance methods added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 class Drone
 {
+    /********Constructor*********/
     constructor(id,name)
     {
+        //instance properties
         this.id = id;
         this.name = name;
     }
+
+    /********Methods*********/
+
+    //Instance method
+    fly()
+    {
+        console.log('Drone ' + this.id + ' is flying');
+    }
+
+    //Static method
+    static getCompany()
+    {
+        console.log('in getCompany');
+    }
 }
+//Static access
+Drone.maxHeight = 2000; //static properties
+console.log(Drone.maxHeight);
+console.log(Drone.getCompany());
+
+//Instance access
 let drone = new Drone('A123','Flyer');
-console.log(drone instanceof Drone);
 console.log(drone.id);
 console.log(drone.name);
+drone.fly();
 
