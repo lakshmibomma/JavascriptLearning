856bd33f60fc54c767408e0294e1b8a7613b563d
Lakshmi Bomma <lbomma@riis.com>
2016-10-31 16:16:49 -0400

    created constructors

---
+++ src/app.js
@@ -0,0 +1,13 @@
+class Drone
+{
+    constructor(id,name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
+let drone = new Drone('A123','Flyer');
+console.log(drone instanceof Drone);
+console.log(drone.id);
+console.log(drone.name);
+
