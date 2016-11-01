e0eada4003375a197a4eed63c5af0ab4be79c08d
Lakshmi Bomma <lbomma@riis.com>
2016-11-01 16:51:56 -0400

    added interface calsses and followed style MDN material design Lite..image,button,titlebar,hamburger menu

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 import {Car} from './classes/car.js';
 import {Drone} from './classes/drone.js';
 import {fleet} from './fleet-data.js';
-import {FleetDataService} from './services/fleet-data-service.js'
+import {FleetDataService} from './services/fleet-data-service.js';
+import $ from 'jquery';
+import {Button} from './ui/button.js';
+import {Image} from './ui/image.js';
+import {TitleBar} from './ui/title-bar.js';
 
-let dataService = new FleetDataService();
-dataService.loadData(fleet);
-console.log(dataService.cars);
+
+// let b = new Button('Click Me');
+// b.appendToElement($('body'));
+
+// let i = new Image('../images/drone.jpg');
+// i.appendToElement($('body'));
+
+let tb = new TitleBar('Our Application');
+tb.addLink('Home','');
+tb.addLink('Cars','');
+tb.addLink('Drones','');
+tb.addLink('Maps','');
+
+tb.appendToElement($('body'));
+
+
+
+
+// let dataService = new FleetDataService();
+// dataService.loadData(fleet);
+// console.log(dataService.cars);
 
 // for (let car of dataService.cars)
 //     console.log(car.license);
@@ -21,9 +43,9 @@ console.log(dataService.cars);
 // for (let car of cars)
 // console.log(car.license)
 
-let cars = dataService.filterCarsByMake('e');
-for (let car of cars)
-  console.log(car.make);
+// let cars = dataService.filterCarsByMake('e');
+// for (let car of cars)
+//   console.log(car.make);
 
 
 
