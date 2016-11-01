ab2af363a8a956119b58dc87ee6e09e1d6edf37b
Lakshmi Bomma <lbomma@riis.com>
2016-11-01 10:23:47 -0400

    inheritance with static methods added in app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,54 @@
 class Vehicle
 {
-    constructor()
+    /** Super class constructor */
+    constructor(licenseNum)
     {
+        this.licenseNum = licenseNum;
+        this.gpsEnabled = true;
         console.log('constructing Vehicle');
     }
 
+   /*Method*/
+    start()
+    {
+        console.log('starting Vehicle');
+    }
+    /**class method/static method */
+    static getCompanyName()
+    {
+        console.log('My Company');
+    }
+
 }
-class Drone extends Vehicle{
+class Drone extends Vehicle
+{
 
 }
 class Car extends  Vehicle
 {
-    constructor()
+    constructor(licenseNum)
     {
-        super();
-        console.log('constructing Car');
+        super(licenseNum); //Super class constructor called first..
+        this.gpsEnabled = false;
+        super.start(); //Super class method can be called  at any time in extended class
     }
+     /*Overridden Method*/
+    start()
+    {
+        console.log('starting Car');
+    }
+
+    /**class method/static method can be overridden*/
+    static getCompanyName()
+    {
+        super.getCompanyName(); //we can super class static method in derived class
+        console.log('My Other Company');
+    }
+
 }
-let c =  new Car();
-console.log(c instanceof Car);
-console.log(c instanceof Vehicle);
-console.log(c instanceof Object);
+let c =  new Car('A123');
+console.log(c.licenseNum);
+console.log(c.gpsEnabled);
+c.start();
+Car.getCompanyName();//Static methods can be accessable in derived class
+
